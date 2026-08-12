3ff3cc7eb92bfe0d29d79882c8c2de2d7d0d60fa
Wendler Vinicius Silva Rodrigues <wendlervinicius9@gmail.com>
2026-08-12 11:04:38 -0300

    feat: adicionar arrays de candidatos e matriz de votos

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
 
 public class SistemaVotacao {
+
+    //variáveis """"globais""""
     static Scanner scan = new Scanner (System.in);
+    static final int MAX_CANDIDATOS = 5;
+    static final int TOTAL_TURMAS = 3;
+    static final int MAX_VOTANTES_POR_TURMA = 10;
+
+    //matrizes
+    static int [] numerosCandidatos = new int [MAX_CANDIDATOS];
+    static String [] nomesCandidatos = new String [MAX_CANDIDATOS];
+    static int [] votosCandidatos = new int [MAX_CANDIDATOS];
+
+    static int [][] votosPorTurma = new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
+
+    static int [] quantidadeVotosTruma = new int [TOTAL_TURMAS];
+    static int quantidadeCandidatos = 0;
+
+    // -------- | --------- | --------
+
+
     public static void main(String[] args) {
+
         System.out.println("Sistema de votação inciado");
+        
         scan.close();
 
 
