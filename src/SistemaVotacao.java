9916ca5dbe3d2e45a4480764a0516f55b89fff14
Wendler Vinicius Silva Rodrigues <wendlervinicius9@gmail.com>
2026-08-28 07:18:56 -0300

    feat: validar seleção da turma

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -165,14 +165,30 @@ public class SistemaVotacao {
     }
 
     //Mostrar os candidatos disponíveis
-    static void mostrarCandidatos(){
+    static void mostrarCandidatos() {
         System.out.println("\nCandidatos disponíveis: ");
 
-        for (int i = 0; i < quantidadeCandidatos; i++){
+        for (int i = 0; i < quantidadeCandidatos; i++) {
             System.out.println(
                     numerosCandidatos[i] + " - " + nomesCandidatos[i]
             );
         }
+
+    }
+
+    static void escolherTurma() {
+        int turma;
+        do {
+            turma = lerInteiro("Informe a turma de 1 a 3: ");
+
+            if (turma < 1 || turma > TOTAL_TURMAS) System.out.println("Turma inválida");
+
+        }while(turma < 1 || turma > TOTAL_TURMAS);
+
+        int indiceTurma = turma -1;
+
+
+
 
     }
 
